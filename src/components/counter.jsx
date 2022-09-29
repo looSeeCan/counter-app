a916c5809626d8cd827b44a8cde6dcbc25fdbf60
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-09-29 16:56:03 -0500

    going to the Wind down on North ave
    stil working on Binding Event Handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'; /// imrc = import statement
 
 /// keyboard shortcut "cc" to creater class
-/// testing: committing to the command line with description and message
 class Counter extends Component {
     state = { /// add a property called state. state is a special property in React component. its basically an object that includes any data that this component needs.
         count: 0,
@@ -9,29 +8,29 @@ class Counter extends Component {
         // imageUrl: "https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/308053926_5403345529781102_4848854497029499904_n.jpg?stp=cp6_dst-jpg&_nc_cat=111&ccb=1-7&_nc_sid=0debeb&_nc_ohc=be9t5WEhZQAAX8X8-81&tn=BVHjSxUsbjfysZKz&_nc_ht=scontent-ord5-1.xx&oh=00_AT_A-wen1jSfH3ENZqzWM04IGVERID_eYNoPtI3TViQ1kg&oe=63340388"
         // tags: ["tag1", "tag2", "tag3",],
         tags: [],
-
+        
     };
-
+    
     // styles = { embeded css style={this.styles}
     //     fontSize: 50,
     //     fontWeight: "bold"
     // };
     
-    render() {
-        let classes = this.getBadgeClasses(); /// refactored
+    // constructor() {
+    //     super();
+    //     // console.log(this.handleIncrement.bind(this));
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
 
-        return ( /// React.Fragment. Get's rid of unecessary divs in html.
-        <React.Fragment> {/*inside here we can add any valid JavaScript expression or here I am using it for a comment*/}
-            {/* <img src={this.state.imageUrl} /> */}
-            <span className={classes}>{this.formatCount()}</span> {}
-            <button className='btn btn-secondary btn-sm'>Increment</button> {/*style ={{fontSize: 30}} = inline */}
-            
-            {this.state.tags.length === 0 && "Please create a new tag!"}
-            {this.renderTags()}
-        </React.Fragment>    
-        );
+    // handleIncrement() {/// binding event handlers
+    //     console.log("Increment clicked:", this);/// this is undefined here, if you do not have the constructor.
+    //     console.log(this.state.count)
+    // }
+
+    handleIncrement = () => { ///instead of doing the whole binding thing. use => functions. they dont rebind the this keyword, they inherit it
+        console.log("Increment clicked:", this);/// this is undefined here, if you do not have the constructor.
     }
-
+    
     renderTags() { /// conditional rendering
         /// (this.state.tags.length === 0) ? <p>There are no tags!</p> : <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
         /// rendering list of items in react. whenever we rendeer a map method, we need to set a key attribute and set the value to a dynamic value {}. the value needs to be unique.
@@ -50,7 +49,27 @@ class Counter extends Component {
         // console.log(count);
         return count === 0 ? 'Zero': count;
         // return this.state.count === 0 ? "Zero": this.state.count;
-    };
+    }
+
+
+    
+    render() {
+        let classes = this.getBadgeClasses(); /// refactored
+
+        return ( /// React.Fragment. Get's rid of unecessary divs in html.
+            <React.Fragment> {/*inside here we can add any valid JavaScript expression or here I am using it for a comment*/}
+                {/* <img src={this.state.imageUrl} /> */}
+                <span className={classes}>{this.formatCount()}</span> {}
+                <button onClick = {this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button> {/*style ={{fontSize: 30}} = inline */}
+                
+                {this.state.tags.length === 0  && "Please create a new tag!"}{/*truthy falsy. if the first statement is true
+                    the && operator  asks if the next is true. in this case it is, because a string is true. prints the string to the screen */}
+                {/* {this.state.tags.length === 0 ? "Please create a new tag!" : "false"} */}
+                {this.renderTags()}
+            </React.Fragment>    
+        );
+    }
+
     
 };
 
